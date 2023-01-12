a07f5edfbbb97f3a18d237570dcde3d11dc1da96
isabeldiana <isabel_diana@outlook.com>
2023-01-11 22:48:52 -0300

    Add delete route

--- routes.js
+++ routes.js
@@ -3,19 +3,41 @@ const cors = require('cors')
 const app = express();
 const {Products} = require('./app/models');
 
+
 app.use(cors())
 
 app.use(express.json())
 
-app.get('/products', async (req, res) => {
-    Products.findAll().then((result) => res.json(result))
+app.get('/products', async (req, res) => { 
+    try {
+        const productsList = await Products.findAll();
+        return res.json(productsList);
+    } catch (error) {
+        return console.error("Error na busca", error);
+    }
+    
 });
 
 app.post('/products', async (req, res) => {
-    const products = await Products.create(req.body)
-    return res.json(products);
+    const {products, amount} = req.body;
+    try {
+        const productsList = await Products.create( {products, amount});
+        return res.json(productsList);
+    } catch (error) {
+        return console.error("Error ao adicionar", error);
+    }
+    
 });
 
+app.delete('/products/:id', async (req, res) => {
+   try {
+        await Products.destroy({where: {id: req.params.id}});
+        return res.json({message: `Exclusao do item ID ${req.params.id} feita com sucesso`});
+   } catch (error) {
+        return console.err("Error na exclusao", error);
+   }
+    
+});
     
 app.listen(3333)
 
